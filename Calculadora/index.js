ee3ee0b409cb00a64fcf6a5da5734fe278dc896e
Gismi Guimarães <97984496+Gismii@users.noreply.github.com>
2022-11-16 01:12:50 -0300

    CalculadoraUp

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -12,34 +12,35 @@ const calculadora = function(calculadora) {
     };
  
     
-do{    
+do{   
+
 const sinal = window.prompt("Digite um dos sinais para realizar a operação: soma +, subtração -, multiplicação *, divisão real /, divisão inteira %, potenciação ** ");
 calculadora();
 
    switch (sinal) {
 
         case "+":
-        window.alert(`A soma de ${n1} e ${n2} é igual ${parseInt(n1+n2)}!!`);
+        window.alert(`A soma de ${n1} e ${n2} é igual ${n1+n2}!!`);
         break;
 
         case "-":
-        window.alert(`A subtração de ${n1} com ${n2} é igual ${parseInt(n1-n2)}!!`);
+        window.alert(`A subtração de ${n1} com ${n2} é igual ${n1-n2}!!`);
         break;
 
         case "*":
-        window.alert(`A multiplicação de  ${n1} por ${n2} é igual ${parseInt(n1*n2)}!!`);
+        window.alert(`A multiplicação de  ${n1} por ${n2} é igual ${n1*n2}!!`);
         break;
 
         case "/":
-        window.alert(`A divisão real entre ${n1} e ${n2} é igual ${parseInt(n1/n2)}!!`);
+        window.alert(`A divisão real entre ${n1} e ${n2} é igual ${n1/n2}!!`);
         break;
 
         case "%":
-        window.alert(`A divisão inteira entre ${n1} e ${n2} é igual ${parseInt(n1%n2)}!!`);
+        window.alert(`A divisão inteira entre ${n1} e ${n2} é igual ${1%n2}!!`);
         break;
 
         case "**":
-        window.alert(`A exponenciação de  ${n1} e ${n2} é igual ${parseInt(n1**n2)}!!`);
+        window.alert(`A exponenciação de ${n1} e ${n2} é igual ${n1**n2}!!`);
         break;
    
     default: alert("Digite o operador correspondente a operação desejada!!")
