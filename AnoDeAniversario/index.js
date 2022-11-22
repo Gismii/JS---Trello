2ed99f50aa1c41214bf3dff23a961945cf0578a9
Gismi Guimarães <97984496+Gismii@users.noreply.github.com>
2022-11-21 21:47:07 -0300

    AnoAniversário

---
+++ AnoDeAniversario/index.js
@@ -0,0 +1,15 @@
+const birthYear =1994;
+const currentYear =2022; 
+
+
+while ( birthYear < 2022){
+
+     for(var i =1; i<currentYear; i++){
+
+        birthYear = birthYear + i;
+
+     }
+
+     console.log(birthYear)
+}
+
--- For-forOf-forIn/iondex.js
+++ For-forOf-forIn/iondex.js
@@ -1,1 +1,39 @@
 const numbers = [2, 4, 8, 16, 32, 64,128, 256, 512, 1024, 2048];
+
+    let soma =0;
+
+    let media =0;
+
+    for(let i = 0; i < numbers.length; i++) {
+
+       soma += numbers[i];
+       media = soma/numbers.length;
+
+       
+    };
+
+    
+   alert(`A soma do ${numbers} é igual a ${soma}!`);
+   alert(`A media dos valores somados é de ${media.toFixed(2)}`);
+ 
+   const notas = {
+      bimestre1: 7.0,
+      bimestre2: 3.7,
+      bimestre3: 6.4,
+      bimestre4: 8.6
+      }
+  let somas =0;
+  let quantidades = 0;
+  for(let bimestre in notas){
+  somas+=notas[bimestre]
+  quantidades++
+  }
+  let mediaa;
+  media = (somas/quantidades).toFixed(2);
+  console.log(somas);
+  
+  if(mediaa >= 7){
+      console.log(`O aluno foi aprovado`)
+  } else {
+      console.log(`O aluno nao foi aprovado com a nota ${media}` )
+  }
